7158efdd2f4f8c6bb9061e26dbca1d7bde5d281b
Ryan <ryzhangofficial@gmail.com>
2025-02-19 10:32:07 -0500

    Add Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,36 +5,36 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-    public static final int kArcadeDriveSpeedAxis = 1;
-    public static final int kArcadeDriveTurnAxis = 2;
+  public static final class DriveConstants {
+    public static final int LEFT_LEADER_ID = 0;
+    public static final int LEFT_FOLLOWER_ID = 1;
+    public static final int RIGHT_LEADER_ID = 2;
+    public static final int RIGHT_FOLLOWER_ID = 3;
+
+    public static final int DRIVE_MOTOR_CURRENT_LIMIT = 60;
   }
 
-  public static class DrivetrainConstants {
-    public static final int kMotorControllerPort0LeftLeader = 0;
-    public static final int kMotorControllerPort1LeftFollower = 1;
-    public static final int kMotorControllerPort2RightLeader = 2;
-    public static final int kMotorControllerPort3RightFollower = 3;
+  public static final class RollerConstants {
+    public static final int ROLLER_MOTOR_ID = 4;
+    public static final int ROLLER_MOTOR_CURRENT_LIMIT = 60;
+    public static final double ROLLER_MOTOR_VOLTAGE_COMP = 10;
+    public static final double ROLLER_EJECT_VALUE = 0.44;
   }
 
-  public static class IntakeConstants {
-    public static final int kIntakeMotorPort = 4;
-    public static final double kIntakeMotorSpeed = .5;
-  }
-
-  public static class AutonomousConstants {
-    public static final double kAutoDriveForwardSpeed = .5;
-    public static final double kAutoDriveIntervalSpeed = .5;
-
-    public static final double kAutoIntakeDuration = 5;
+  public static final class OperatorConstants {
+    public static final int DRIVER_CONTROLLER_PORT = 0;
+    public static final int OPERATOR_CONTROLLER_PORT = 1;
   }
 }
