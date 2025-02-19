ff3f4d0c1bece8542e9447093c8de755f5e283a9
Ryan <ryzhangofficial@gmail.com>
2025-02-19 10:30:31 -0500

    Update RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,69 +4,97 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.teleoperated.ArcadeDrive;
-import frc.robot.commands.teleoperated.IntakeCommand;
-import frc.robot.commands.teleoperated.ReverseIntakeCommand;
-import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Intake;
-import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.networktables.GenericEntry;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants.RollerConstants;
+import frc.robot.commands.AutoCommand;
+import frc.robot.commands.DriveCommand;
+import frc.robot.commands.RollerCommand;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.RollerSubsystem;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final GenericEntry joystickEntry;
-  // Joystick or Controller
-  private final Joystick m_driverController =
-      new Joystick(OperatorConstants.kDriverControllerPort);
-      
-  private final Drivetrain m_drivetrain = new Drivetrain(m_driverController);
-  private final Intake m_intake = new Intake();
+  // The robot's subsystems
+  private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+  private final RollerSubsystem rollerSubsystem = new RollerSubsystem();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  // The driver's controller
+  private final CommandXboxController driverController = new CommandXboxController(
+      OperatorConstants.DRIVER_CONTROLLER_PORT);
+
+  // The operator's controller
+  private final CommandXboxController operatorController = new CommandXboxController(
+      OperatorConstants.OPERATOR_CONTROLLER_PORT);
+
+  // The autonomous chooser
+  private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
-    // Configure the trigger bindings
+    // Set up command bindings
     configureBindings();
-    ShuffleboardTab joystickTab = Shuffleboard.getTab("Joystick Tab");
-    joystickEntry = joystickTab.add("Joystick", 0).getEntry();
 
-    System.out.println("DEFAULT ARCADE DRIVE COMMAND! (TELEOPERATED)!");
-    m_drivetrain.setDefaultCommand(new ArcadeDrive(m_drivetrain,
-      () -> -m_driverController.getRawAxis(OperatorConstants.kArcadeDriveSpeedAxis),
-      () -> m_driverController.getRawAxis(OperatorConstants.kArcadeDriveTurnAxis)
-    ));
+    // Set the options to show up in the Dashboard for selecting auto modes. If you
+    // add additional auto modes you can add additional lines here with
+    // autoChooser.addOption
+    autoChooser.setDefaultOption("Autonomous", new AutoCommand(driveSubsystem));
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
-    new JoystickButton(m_driverController, 5)
-                      .whileTrue(new IntakeCommand(m_intake));
-    new JoystickButton(m_driverController, 6)
-                      .whileTrue(new ReverseIntakeCommand(m_intake));
+    // Set the A button to run the "RollerCommand" command with a fixed
+    // value ejecting the gamepiece while the button is held
+
+    // before
+    operatorController.a()
+        .whileTrue(new RollerCommand(() -> RollerConstants.ROLLER_EJECT_VALUE, () -> 0, rollerSubsystem));
+
+    // Set the default command for the drive subsystem to an instance of the
+    // DriveCommand with the values provided by the joystick axes on the driver
+    // controller. The Y axis of the controller is inverted so that pushing the
+    // stick away from you (a negative value) drives the robot forwards (a positive
+    // value). Similarly for the X axis where we need to flip the value so the
+    // joystick matches the WPILib convention of counter-clockwise positive
+    driveSubsystem.setDefaultCommand(new DriveCommand(
+        () -> -driverController.getLeftY() *
+            (driverController.getHID().getRightBumperButton() ? 1 : 0.5),
+        () -> -driverController.getRightX(),
+        driveSubsystem));
+
+    // Set the default command for the roller subsystem to an instance of
+    // RollerCommand with the values provided by the triggers on the operator
+    // controller
+    rollerSubsystem.setDefaultCommand(new RollerCommand(
+        () -> operatorController.getRightTriggerAxis(),
+        () -> operatorController.getLeftTriggerAxis(),
+        rollerSubsystem));
   }
 
   /**
@@ -74,16 +102,8 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getDriveForwardAuto() {
+  public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.driveForwardAuto(m_drivetrain);
-  }
-
-  public Command getDriveIntervalAuto() {
-    return Autos.driveIntervalAuto(m_drivetrain);
-  }
-  
-  public Command getIntakeAuto() {
-    return Autos.intakeAuto(m_intake);
+    return autoChooser.getSelected();
   }
 }
