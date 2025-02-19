354aab10fac05f7dc26bdc2e6fa3d0dcae72d4f6
Ryan <ryzhangofficial@gmail.com>
2025-02-19 10:30:18 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,129 +2,100 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-                                    
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.Constants.DrivetrainConstants;
+package frc.robot;
 
 import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.hal.FRCNetComm.tResourceType;
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
-    private final AHRS ahrs = new AHRS(SPI.Port.kMXP);
+  private Command m_autonomousCommand;
 
-  /*
-   * Initialize the motors for the Differential Drivetrain within the Robots Class
-   */
-    public final static PWMSparkMax leftLeader = new PWMSparkMax(DrivetrainConstants.kMotorControllerPort0LeftLeader);
-    public final static PWMSparkMax leftFollower = new PWMSparkMax(DrivetrainConstants.kMotorControllerPort1LeftFollower);
-    public final static PWMSparkMax rightLeader = new PWMSparkMax(DrivetrainConstants.kMotorControllerPort2RightLeader);
-    public final static PWMSparkMax rightFollower = new PWMSparkMax(DrivetrainConstants.kMotorControllerPort3RightFollower);
-    public DifferentialDrive m_robotDrive;
-        
-        
-          private Command m_driveForwardAuto;
-          private Command m_driveIntervalAuto;
-        
-          private RobotContainer m_robotContainer;
-        
-  public Robot() {
-    leftLeader.setSafetyEnabled(true);
-    leftFollower.setSafetyEnabled(true);
-    rightLeader.setSafetyEnabled(true);
-    rightFollower.setSafetyEnabled(true);
-    
-  }
-    
-      public static PWMSparkMax getLeftLeader(){
-        return leftLeader;
-      }
-      public static PWMSparkMax getRightLeader(){
-        return rightLeader;
-      }
-      public static PWMSparkMax getLeftFollower(){
-        return leftFollower;
-      }
-      public static PWMSparkMax getRightFollower(){
-        return rightFollower;
-      }
+  private RobotContainer m_robotContainer;
 
-
-
+  private final AHRS ahrs = new AHRS(SPI.Port.kMXP);
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
     // autonomous chooser on the dashboard.
-    
-    System.out.println(String.format("[i] navX Connected: %s", ahrs.isConnected()));
-
     m_robotContainer = new RobotContainer();
 
-    /*
-     * Sets up the differential drive aspect of the code
-     */
-    leftLeader.addFollower(leftFollower);
-    rightLeader.addFollower(rightFollower);
-
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    rightLeader.setInverted(true);
-
-    m_robotDrive = new DifferentialDrive(leftLeader::set, rightLeader::set);
+    // Used to track usage of the KitBot code, please do not remove
+    HAL.report(tResourceType.kResourceType_Framework, 9);
+    
+    System.out.println(String.format("[i] navX Connected: %s", ahrs.isConnected()));
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  /**
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
-    m_driveForwardAuto = m_robotContainer.getDriveForwardAuto();
-    m_driveIntervalAuto = m_robotContainer.getDriveIntervalAuto();
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
     // schedule the autonomous command (example)
-    if (m_driveForwardAuto != null) {
-      m_driveForwardAuto.schedule();
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
     }
   }
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
   public void teleopInit() {
@@ -132,14 +103,15 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    if (m_driveForwardAuto != null) {
-      m_driveForwardAuto.cancel();
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
   }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
   public void testInit() {
@@ -149,13 +121,16 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   /** This function is called once when the robot is first started up. */
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+  }
 }
